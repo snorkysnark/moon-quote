9a188ac66395fbd18727edaff34c533d1d5eafa6
snorkysnark <snorkysnark@gmail.com>
2022-12-09 14:56:14 +0100

    split EpubDisplay into rendition + view

--- src/reader/EpubRendition.tsx
+++ src/reader/EpubRendition.tsx
@@ -12,6 +12,7 @@ import {
 } from "solid-js";
 import { createBlobUrl, revokeBlobUrl } from "epubjs/src/utils/core";
 import { EVENTS } from "epubjs/src/utils/constants";
+import EpubView from "./EpubView";
 
 export interface Controller {
     display: (target: number) => void;
@@ -35,62 +36,14 @@ export default function EpubRendition(props: {
     );
 
     const [section, setSection] = createSignal<Section>(null);
-    // When the section changes, reset the associated values
-    createEffect(
-        on(section, () => {
-            onCleanup(() => {
-                setContents(null);
-                startAtBottom = false;
-            });
-        })
-    );
 
     // @ts-ignore: wrong type signature for prev()
     const prevSection = () => section()?.prev() as Section;
     // @ts-ignore: wrong type signature for next()
     const nextSection = () => section()?.next() as Section;
 
-    let startAtBottom = false;
-
-    const [contents, setContents] = createSignal<Contents>(null);
-    createEffect(
-        on(contents, () => {
-            if (contents()) {
-                const lastContents = contents();
-                const onResize = () => {
-                    setTextHeight(lastContents.textHeight());
-                    if (startAtBottom) {
-                        scroller.scrollTop = lastContents.textHeight();
-                        startAtBottom = false;
-                    }
-                };
-                lastContents.on(EVENTS.CONTENTS.RESIZE, onResize);
-
-                onCleanup(() => {
-                    lastContents.off(EVENTS.CONTENTS.RESIZE, onResize);
-                    lastContents.destroy();
-                    setTextHeight(null);
-                });
-            }
-        })
-    );
-    const [textHeight, setTextHeight] = createSignal<number>(null);
-
     // Load section content into a Blob
     const request = createMemo(() => (path: string) => props.epub.load(path));
-    const [html] = createResource(
-        () => (section() ? { section: section(), request: request() } : null),
-        ({ section, request }) => {
-            return section.render(request);
-        }
-    );
-    const blobUrl = createMemo(() => {
-        if (html()) {
-            const url = createBlobUrl(html(), "application/xhtml+xml");
-            onCleanup(() => revokeBlobUrl(url));
-            return url;
-        }
-    });
 
     const controller: Controller = {
         display: (target: number) => {
@@ -99,7 +52,6 @@ export default function EpubRendition(props: {
         prev: () => {
             if (prevSection()) {
                 setSection(prevSection());
-                startAtBottom = true;
             }
         },
         next: () => {
@@ -108,60 +60,9 @@ export default function EpubRendition(props: {
     };
     if (props.setController) props.setController(controller);
 
-    const onKeyDown = (event: KeyboardEvent) => {
-        event.preventDefault();
-        switch (event.key) {
-            case "ArrowLeft":
-                controller.prev();
-                break;
-            case "ArrowRight":
-                controller.next();
-                break;
-        }
-    };
-
-    let scroller: HTMLDivElement;
-    let iframe: HTMLIFrameElement;
-    onMount(() => {
-        window.addEventListener("keydown", onKeyDown);
-
-        onCleanup(() => {
-            window.removeEventListener("keydown", onKeyDown);
-        });
-    });
-    const onLoadIframe = () => {
-        // No need to removeEventListener, since document gets unloaded anyway
-        iframe.contentWindow.addEventListener("keydown", onKeyDown);
-
-        const iframeDoc = iframe.contentDocument;
-        iframeDoc.body.style.overflow = "hidden";
-
-        setContents(
-            new Contents(
-                iframeDoc,
-                iframeDoc.body,
-                section().cfiBase,
-                section().index
-            )
-        );
-    };
-
     return (
-        <div class="w-full h-full overflow-scroll relative" ref={scroller}>
-            <div class="absolute w-10 h-10 top-10 left-10 bg-red-500 pointer-events-none"></div>
-            <Show when={blobUrl()}>
-                <iframe
-                    class="w-full overflow-hidden"
-                    src={blobUrl()}
-                    ref={iframe}
-                    onLoad={onLoadIframe}
-                    style={{
-                        height: textHeight()
-                            ? `max(${textHeight()}px, 100%)`
-                            : "100%",
-                    }}
-                ></iframe>
-            </Show>
-        </div>
+        <Show when={section()}>
+            <EpubView request={request()} section={section()} />
+        </Show>
     );
 }
---
+++ src/reader/EpubView.tsx
@@ -0,0 +1,90 @@
+import { Contents } from "epubjs";
+import Section from "epubjs/types/section";
+import { createBlobUrl, revokeBlobUrl } from "epubjs/src/utils/core";
+import {
+    batch,
+    createEffect,
+    createMemo,
+    createResource,
+    createSignal,
+    on,
+    onCleanup,
+    Show,
+} from "solid-js";
+import { EVENTS } from "epubjs/src/utils/constants";
+
+export default function EpubView(props: {
+    request: (path: string) => Promise<any>;
+    section: Section;
+}) {
+    const [html] = createResource(
+        () => ({ request: props.request, section: props.section }),
+        ({ section, request }) => {
+            return section.render(request);
+        }
+    );
+    const blobUrl = createMemo(() => {
+        if (html()) {
+            const url = createBlobUrl(html(), "application/xhtml+xml");
+            onCleanup(() => {
+                revokeBlobUrl(url);
+            });
+            return url;
+        }
+    });
+
+    // Not cleaning up contents/textHeight when section changes,
+    // so that iframe doesn't flicker while loading new content
+    const [contents, setContents] = createSignal<Contents>(null);
+    const [textHeight, setTextHeight] = createSignal<number>(null);
+    createEffect(
+        on(contents, () => {
+            if (contents()) {
+                const lastContents = contents();
+                const onResize = () => {
+                    setTextHeight(lastContents.textHeight());
+                };
+                lastContents.on(EVENTS.CONTENTS.RESIZE, onResize);
+
+                onCleanup(() => {
+                    lastContents.off(EVENTS.CONTENTS.RESIZE, onResize);
+                    lastContents.destroy();
+                });
+            }
+        })
+    );
+
+    let scroller: HTMLDivElement;
+    let iframe: HTMLIFrameElement;
+    const onLoadIframe = () => {
+        const iframeDoc = iframe.contentDocument;
+        iframeDoc.body.style.overflow = "hidden";
+
+        setContents(
+            new Contents(
+                iframeDoc,
+                iframeDoc.body,
+                props.section.cfiBase,
+                props.section.index
+            )
+        );
+    };
+
+    return (
+        <div class="w-full h-full overflow-scroll relative" ref={scroller}>
+            <Show when={blobUrl()}>
+                <iframe
+                    class="w-full overflow-hidden"
+                    src={blobUrl()}
+                    onLoad={onLoadIframe}
+                    ref={iframe}
+                    style={{
+                        height: textHeight()
+                            ? `max(${textHeight()}px, 100%)`
+                            : "100%",
+                    }}
+                ></iframe>
+            </Show>
+        </div>
+    );
+}
