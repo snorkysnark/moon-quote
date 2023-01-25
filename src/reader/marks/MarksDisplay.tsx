050538fe7fbe8d00ad5ee142405ee49a32ac57c4
snorkysnark <snorkysnark@gmail.com>
2023-01-25 22:56:21 +0100

    display markers with flag icon

--- src/reader/marks/MarksDisplay.tsx
+++ src/reader/marks/MarksDisplay.tsx
@@ -1,42 +1,57 @@
 import { For } from "solid-js";
 import { Highlight, Marker } from "./marks";
+import { BsFlagFill } from 'solid-icons/bs'
 
 export default function MarksDisplay(props: {
     highlights: Highlight[];
     markers: Marker[];
 }) {
     return (
-        <svg class="absolute overflow-visible pointer-events-none mix-blend-multiply">
-            <For each={props.highlights}>
-                {(mark) => (
-                    <g class="fill-yellow-200">
-                        <For each={mark.clientRects}>
-                            {(clientRect) => (
-                                <rect
-                                    x={clientRect.x}
-                                    y={clientRect.y}
-                                    width={clientRect.width}
-                                    height={clientRect.height}
-                                ></rect>
-                            )}
-                        </For>
-                    </g>
-                )}
-            </For>
+        <>
+            <svg class="absolute overflow-visible pointer-events-none mix-blend-multiply">
+                <For each={props.highlights}>
+                    {(mark) => (
+                        <g class="fill-yellow-200">
+                            <For each={mark.clientRects}>
+                                {(clientRect) => (
+                                    <rect
+                                        x={clientRect.x}
+                                        y={clientRect.y}
+                                        width={clientRect.width}
+                                        height={clientRect.height}
+                                    ></rect>
+                                )}
+                            </For>
+                        </g>
+                    )}
+                </For>
+
+                <For each={props.markers}>
+                    {(marker) => (
+                        <rect
+                            class="fill-red-500"
+                            x={marker.rect.x - 1}
+                            y={marker.rect.y}
+                            width="3"
+                            height={marker.rect.height}
+                        ></rect>
+                    )}
+                </For>
+            </svg>
 
             <For each={props.markers}>
                 {(marker) => (
-                    <g class="fill-red-500">
-                        <rect x={marker.rect.x - 4} y={marker.rect.y - 6} width="8" height="8"></rect>
-                        <rect
-                            x={marker.rect.x - 1}
-                            y={marker.rect.y}
-                            width="2"
-                            height={marker.rect.height}
-                        ></rect>
-                    </g>
+                    <div
+                        class="absolute"
+                        style={{
+                            left: `${marker.rect.x - 1}px`,
+                            top: `${marker.rect.y}px`,
+                        }}
+                    >
+                        <BsFlagFill color="red" class="mix-blend-multiply" />
+                    </div>
                 )}
             </For>
-        </svg>
+        </>
     );
 }
