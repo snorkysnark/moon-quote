f82468cbd541e660608c6e2df50ebc95df0c9ab4
snorkysnark <snorkysnark@gmail.com>
2023-02-02 13:28:58 +0100

    remove 'exporters' folder

--- src-tauri/exporters/JSON.js
+++
@@ -1,7 +0,0 @@
-export function serialize(book) {
-    return {
-        content: JSON.stringify(book, null, 4),
-        language: "json",
-        extension: "json",
-    };
-}
--- src-tauri/src/exporters/mod.rs
+++
@@ -1,6 +0,0 @@
-mod default_scripts;
-mod get_scripts;
-mod plugin;
-
-pub use default_scripts::create_exporters_dir;
-pub use plugin::ExportersPlugin;
