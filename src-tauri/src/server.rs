7c4262fc9460c9e2c20c968d8149853f69893be7
snorkysnark <snorkysnark@gmail.com>
2023-02-09 21:15:35 +0100

    websocket server

--- src-tauri/src/server.rs
+++ src-tauri/src/server.rs
@@ -1,4 +1,9 @@
-use axum::{http::Method, routing::get, Router};
+use axum::{
+    extract::ws::{Message, WebSocket, WebSocketUpgrade},
+    http::Method,
+    routing::get,
+    Router,
+};
 use tauri::{
     plugin::{Builder, TauriPlugin},
     Runtime,
@@ -6,12 +11,32 @@ use tauri::{
 use tower_http::cors::{Any, CorsLayer};
 
 pub fn plugin<R: Runtime>() -> TauriPlugin<R> {
+    async fn handle_socket(mut socket: WebSocket) {
+        while let Some(msg) = socket.recv().await {
+            match msg {
+                Ok(Message::Text(text)) => {
+                    if &text == "choose_quote" {
+                        println!("Opening quote dialog");
+                    }
+                }
+                Err(_) => {
+                    // client disconnected
+                    return;
+                }
+                _ => {}
+            }
+        }
+    }
+
     Builder::new("server")
         .setup(|_| {
             tauri::async_runtime::spawn(async {
                 // build our application with a single route
                 let app = Router::new()
-                    .route("/", get(|| async { "Hello, World!" }))
+                    .route(
+                        "/",
+                        get(|ws: WebSocketUpgrade| async { ws.on_upgrade(handle_socket) }),
+                    )
                     .layer(
                         CorsLayer::new()
                             .allow_methods([Method::GET])
@@ -19,7 +44,7 @@ pub fn plugin<R: Runtime>() -> TauriPlugin<R> {
                     );
 
                 // run it with hyper on localhost:3000
-                axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+                axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
                     .serve(app.into_make_service())
                     .await
                     .unwrap();
