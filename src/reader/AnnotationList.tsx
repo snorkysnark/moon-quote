0e2c14b3ebe62deff1691214019749c8f6988d10
snorkysnark <snorkysnark@gmail.com>
2023-03-30 14:34:03 +0200

    split long class string

--- src/reader/AnnotationList.tsx
+++ src/reader/AnnotationList.tsx
@@ -37,7 +37,8 @@ export default function AnnotationList(props: {
                                 }
                             >
                                 <div
-                                    class="px-1 border-2 rounded border-dashed bg-blue-100 border-blue-500 focus:border-solid focus:bg-yellow-100"
+                                    class="px-1 border-2 rounded border-dashed bg-blue-100 \
+                                    border-blue-500 focus:border-solid focus:bg-yellow-100"
                                     data-placeholder="Add note"
                                     contentEditable={selected(annotation)}
                                     onInput={(event) => {
